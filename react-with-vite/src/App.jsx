667f9cc23cea359c0dc7bbefa0ecffdbdfbf2995
Victor <victornakaba@gmail.com>
2025-05-24 05:10:29 -0300

    Scroll ir pro topo quando ir para outra pagina no footer

--- react-with-vite/src/App.jsx
+++ react-with-vite/src/App.jsx
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
-import Home from './pages/home'
-import Project from './pages/projects'
-import Contact from './pages/contact'
-import About from './pages/about'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/home";
+import Project from "./pages/projects";
+import Contact from "./pages/contact";
+import About from "./pages/about";
+import ScrollTop from "./utils/Scrollup";
 
 function App() {
-
   return (
     <Router>
+      <ScrollTop />
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/contact' element={<Contact />}></Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='/projects' element={<Project/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/projects" element={<Project />}></Route>
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;

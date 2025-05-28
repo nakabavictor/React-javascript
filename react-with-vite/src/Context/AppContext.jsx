8643992372967f9bfee045e476f2f5e8f0377042
Victor <victornakaba@gmail.com>
2025-05-28 18:12:51 -0300

    Criacao do context para mudanca de lingua, coloquei tambem o apprpovider d context no main

---
+++ react-with-vite/src/Context/AppContext.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect, createContext } from "react";
+import getApiData from "../Services/apiServices";
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [language, setLanguage] = useState("br");
+  const [languages, setLanguages] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchlanguges = async () => {
+      try {
+        const getTexts = await getApiData("webtext");
+        setLanguages(getTexts);
+        setLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchlanguges();
+  }, []);
+
+  return <AppContext.Provider value={{ language, languages, setLanguage, loading }}>{children}</AppContext.Provider>;
+};
